fbb54385b614497b697acd7c8f16cddf02a2bf4c
andrew <andandiv@gmail.com>
2025-06-26 02:07:36 +0300

    second commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("checkstyle")
 }
 
 group = "org.example"
@@ -12,6 +13,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+checkstyle {
+    toolVersion = "10.15.0"
+    configFile = file("${rootProject.projectDir}/checkstyle.xml")
 }
 
 tasks.test {
